e2085b7c2704e910031460554a24974fafdb878c
Evan <brobergevan@gmail.com>
2024-11-11 20:14:43 -0800

    proper commit

---
+++ stock_handler.gd
@@ -0,0 +1,12 @@
+extends Node
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+
+func dostuff():
+	print("stuff done")
+	pass
