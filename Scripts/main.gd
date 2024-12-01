4650d4016d15622f924d4aef236c90343d1c6c54
Meoah <liyen-liu@hotmail.com>
2024-12-01 14:42:01 -0800

    partially broken

--- Scripts/main.gd
+++ Scripts/main.gd
@@ -15,13 +15,21 @@ func _ready():
 	
 	## TODO Prevent double stock impact on initialization
 	
+	## Initializes each stock to random unique stock and a value between $100 and $500
 	for eachStock in stockNodes:
 		var name = stockChoices.pop_at(randi() % stockChoices.size())
 		eachStock.nameLabel.text = name
-		
+		eachStock.value = randf_range(100.0,500.0)
+	
+	## TODO Delayed effect of first four news?
+	## Initializes first four news posts with no stock doubles.
 	for eachNews in newsNodes:
 		var news = randi_range(1,54)
-		while news in activeNews : news = randi_range(1,54)
+		
+		#Checks if duplicate post or duplicate stock.
+		
+		while news in activeNews: 
+			news = randi_range(1,54)
 		eachNews.newsFlash(news)
 		var newsData = eachNews.newsFlash(news)
 		
@@ -30,3 +38,9 @@ func _ready():
 		
 		activeNews.append(news)
 		
+func preventDuplicateNews(news):
+	## Checks if duplicate post or duplicate stock impact.
+	var activeNewsType : Array
+	for each in activeNews:
+		activeNewsType.append(stocksNodes)
+	pass
