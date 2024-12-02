ac5d9d5f77965c5191c9e3e08fca831391441aab
Silasfay <waffleiron58@gmail.com>
2024-12-01 19:16:22 -0800

    added multiplayer
    Added controller support and multiplayer plugin

--- Scripts/menu.gd
+++ Scripts/menu.gd
@@ -1,6 +1,7 @@
 extends Control
 
 @export var StartingButton :Button
+@export_file var MainScene 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	StartingButton.grab_focus()
@@ -12,4 +13,4 @@ func _process(delta: float) -> void:
 
 
 func _on_start_button_pressed() -> void:
-	get_tree().change_scene_to_file("res://scenes/main.tscn")
+	get_tree().change_scene_to_file(MainScene)
--- Scripts/player.gd
+++ Scripts/player.gd
@@ -1,6 +1,8 @@
 class_name Player extends CharacterBody2D
 
-var speed := 500 #Pixels per second
+@export var speed := 500 #Pixels per second
+@export var device :int
+
 
 var hoveredStock = null
 var Portf 
@@ -12,13 +14,7 @@ var portfolio = [
 ]
 var currency = 2000
 
-func setHoverignStock(stock:Stock) -> void:
-	if stock is Stock:
-		print(stock.stock_id)
-	elif stock == null:
-		print(-1)
-		
-	hoveredStock = stock
+
 
 
 func _ready() -> void:
@@ -27,11 +23,11 @@ func _ready() -> void:
 
 
 func _process(delta: float) -> void:
-	var movementDirection = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	var movementDirection = MultiplayerInput.get_vector(device,"up", "right", "left", "down")
 	velocity = movementDirection * speed
 	move_and_slide()
 	#position = (position+movement).clamp()
-	if Input.is_action_just_pressed("buy"):
+	if MultiplayerInput.is_action_just_pressed(device,"buy"):
 		pitch = randf_range(.99,1.1)
 		$AudioStreamPlayer.set_pitch_scale(pitch)
 		$AudioStreamPlayer.play(0)
@@ -46,7 +42,7 @@ func _process(delta: float) -> void:
 			#print(str(portfolio[hoveredStock.stock_id]))
 		else:
 			print("not hovering!")
-	if Input.is_action_just_pressed("sell"):
+	if MultiplayerInput.is_action_just_pressed(device,"sell"):
 		pitch = randf_range(.8,.98)
 		$AudioStreamPlayer.set_pitch_scale(pitch)
 		$AudioStreamPlayer.play(0)
